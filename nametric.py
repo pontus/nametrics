ded54a28e3943efc4a47910b57095d556885b98a
Pontus Freyhult <pontus_github@soua.net>
2023-11-19 14:52:10 +0100

    Set up logger correctly and type fixes

--- nametric.py
+++ nametric.py
@@ -6,88 +6,104 @@ import yaml
 import requests
 import typing
 import time
+import logging
+import logging.handlers
 
+logger = logging.getLogger()
+Gauge = prometheus_client.Gauge
+
+
+class Token(typing.TypedDict):
+    expire_at: float
+    expire_in: float
+    access_token: str
+
+
+class Metrics(typing.TypedDict):
+    nafirmware: Gauge
+    nareachable: Gauge
+    nabattery_vp: Gauge
+    nabattery_percent: Gauge
+    nawifi_status: Gauge
+    narf_status: Gauge
+    nalast_seen: Gauge
+    narain: Gauge
+    nanoise: Gauge
+    nahumidity: Gauge
+    naco2: Gauge
+    natemperature: Gauge
+    naguststrength: Gauge
+    nawgustangle: Gauge
+    napressure: Gauge
+    naabsolutepressure: Gauge
+    nawindstrength: Gauge
+    nawindangle: Gauge
 
 class Meter:
-    metrics : typing.Dict = {}
-    token : typing.Union[typing.Dict,None]= None
+    token: typing.Union[Token, None] = None
 
-    def __init__(self):
+    def __init__(self) -> None:
         with open("config.yaml") as f:
             self.naconfig = yaml.safe_load(f)
 
         self.get_netatmo_token()
 
-        metrics = self.metrics
-
-        metrics["nafirmware"] = prometheus_client.Gauge(
-            "nafirmware", "Netatmo firmware reported", ["name", "id"]
+        self.metrics = Metrics(
+            nafirmware=Gauge("nafirmware", "Netatmo firmware reported", ["name", "id"]),
+            nareachable=Gauge(
+                "nareachable", "Netatmo device is reachable", ["name", "id"]
+            ),
+            nabattery_vp=Gauge(
+                "nabattery_vp", "Netatmo reported battery level (vp)", ["name", "id"]
+            ),
+            nabattery_percent=Gauge(
+                "nabattery_percent",
+                "Netatmo reported battery level (percent)",
+                ["name", "id"],
+            ),
+            nawifi_status=Gauge(
+                "nawifi_status",
+                "Netatmo reported wifi status",
+                ["name", "id"],
+            ),
+            narf_status=Gauge(
+                "narf_status", "Netatmo reported RF status", ["name", "id"]
+            ),
+            nalast_seen=Gauge(
+                "nalast_seen", "Timestamp of device last seen", ["name", "id"]
+            ),
+            nawindstrength=Gauge(
+                "nawindstrength", "Wind strength reported by Netatmo", ["name", "id"]
+            ),
+            nawindangle=Gauge(
+                "nawindangle", "Wind angle reported by Netatmo", ["name", "id"]
+            ),
+            naguststrength=Gauge(
+                "naguststrength", "Gust strength reported by Netatmo", ["name", "id"]
+            ),
+            nawgustangle=Gauge(
+                "nagustangle", "Gust angle reported by Netatmo", ["name", "id"]
+            ),
+            narain=Gauge("narain", "Rain reported by by Netatmo", ["name", "id"]),
+            nahumidity=Gauge(
+                "nahumidity", "Humidity reported by by Netatmo", ["name", "id"]
+            ),
+            nanoise=Gauge("nanoise", "Noise reported by by Netatmo", ["name", "id"]),
+            naco2=Gauge("naco2", "CO2 reported by by Netatmo", ["name", "id"]),
+            natemperature=Gauge(
+                "natemperature", "Temperature reported by by Netatmo", ["name", "id"]
+            ),
+            napressure=Gauge(
+                "napressure", "Pressure reported by by Netatmo", ["name", "id"]
+            ),
+            naabsolutepressure=Gauge(
+                "naabsolutepressure",
+                "Absolute pressure reported by by Netatmo",
+                ["name", "id"],
+            ),
         )
 
-        metrics["nareachable"] = prometheus_client.Gauge(
-            "nareachable", "Netatmo device is reachable", ["name", "id"]
-        )
-        metrics["nabattery_vp"] = prometheus_client.Gauge(
-            "nabattery_vp", "Netatmo reported battery level (vp)", ["name", "id"]
-        )
-        metrics["nabattery_percent"] = prometheus_client.Gauge(
-            "nabattery_percent",
-            "Netatmo reported battery level (percent)",
-            ["name", "id"],
-        )
-
-        metrics["nawifi_status"] = prometheus_client.Gauge(
-            "nawifi_status",
-            "Netatmo reported wifi status",
-            ["name", "id"],
-        )
-
-        metrics["narf_status"] = prometheus_client.Gauge(
-            "narf_status",
-            "Netatmo reported RF status",
-            ["name", "id"],
-        )
-
-        metrics["nalast_seen"] = prometheus_client.Gauge(
-            "nalast_seen", "Timestamp of device last seen", ["name", "id"]
-        )
-        metrics["nawindstrength"] = prometheus_client.Gauge(
-            "nawindstrength", "Wind strength reported by Netatmo", ["name", "id"]
-        )
-        metrics["nawindangle"] = prometheus_client.Gauge(
-            "nawindangle", "Wind angle reported by Netatmo", ["name", "id"]
-        )
-        metrics["naguststrength"] = prometheus_client.Gauge(
-            "naguststrength", "Gust strength reported by Netatmo", ["name", "id"]
-        )
-        metrics["nawgustangle"] = prometheus_client.Gauge(
-            "nagustangle", "Gust angle reported by Netatmo", ["name", "id"]
-        )
-        metrics["narain"] = prometheus_client.Gauge(
-            "narain", "Rain reported by by Netatmo", ["name", "id"]
-        )
-        metrics["nahumidity"] = prometheus_client.Gauge(
-            "nahumidity", "Humidity reported by by Netatmo", ["name", "id"]
-        )
-        metrics["nanoise"] = prometheus_client.Gauge(
-            "nanoise", "Noise reported by by Netatmo", ["name", "id"]
-        )
-        metrics["naco2"] = prometheus_client.Gauge(
-            "naco2", "CO2 reported by by Netatmo", ["name", "id"]
-        )
-        metrics["natemperature"] = prometheus_client.Gauge(
-            "natemperature", "Temperature reported by by Netatmo", ["name", "id"]
-        )
-        metrics["napressure"] = prometheus_client.Gauge(
-            "napressure", "Pressure reported by by Netatmo", ["name", "id"]
-        )
-        metrics["naabsolutepressure"] = prometheus_client.Gauge(
-            "naabsolutepressure", "Absolute pressure reported by by Netatmo", ["name", "id"]
-        )
-
-
-
-    def refresh_all_meters(self):
+    def refresh_all_meters(self) -> None:
         token = self.get_netatmo_token()
         r = requests.request(
             method="GET",
@@ -109,11 +125,13 @@ class Meter:
             self.metrics["nawifi_status"].labels(name=name, id=id).set(p["wifi_status"])
             self.metrics["nareachable"].labels(name=name, id=id).set(p["reachable"])
 
-            if 'dashboard_data' in p:
+            if "dashboard_data" in p:
                 d = p["dashboard_data"]
 
                 self.metrics["nahumidity"].labels(name=name, id=id).set(d["Humidity"])
-                self.metrics["natemperature"].labels(name=name, id=id).set(d["Temperature"])
+                self.metrics["natemperature"].labels(name=name, id=id).set(
+                    d["Temperature"]
+                )
                 self.metrics["nanoise"].labels(name=name, id=id).set(d["Noise"])
                 self.metrics["napressure"].labels(name=name, id=id).set(d["Pressure"])
                 self.metrics["naabsolutepressure"].labels(name=name, id=id).set(
@@ -130,7 +148,7 @@ class Meter:
                 self.metrics["narf_status"].labels(name=name, id=id).set(m["rf_status"])
                 self.metrics["nafirmware"].labels(name=name, id=id).set(m["firmware"])
 
-                if not 'dashboard_data' in m:
+                if not "dashboard_data" in m:
                     continue
 
                 d = m["dashboard_data"]
@@ -156,7 +174,7 @@ class Meter:
                         d["AbsolutePressure"]
                     )
 
-    def get_netatmo_token(self):
+    def get_netatmo_token(self) -> Token:
         if self.token and self.token["expire_at"] < time.time():
             return self.token
 
@@ -181,14 +199,34 @@ class Meter:
         if not r.ok:
             raise SystemError("Failed to refresh token")
 
-        token = r.json()
+        token: Token = r.json()
         token["expire_at"] = t + token["expire_in"]
 
         self.token = token
         return token
 
 
-def serve():
+def setup_logger(
+    console_level: int = logging.DEBUG,
+    file_level: int = logging.DEBUG,
+    filename: str = "nametrics.log",
+) -> None:
+    h = logging.StreamHandler()
+    h.setLevel(console_level)
+    logger.addHandler(h)
+    f = logging.handlers.TimedRotatingFileHandler(
+        filename, when="midnight", backupCount=30
+    )
+    f.setFormatter(logging.Formatter("{asctime} - {levelname} - {message}", style="{"))
+    f.setLevel(file_level)
+    logger.addHandler(f)
+
+    logger.setLevel(min(file_level, console_level))
+
+
+def serve() -> None:
+    setup_logger()
+
     meter = Meter()
     prometheus_client.start_http_server(8012)
     meter.refresh_all_meters()
